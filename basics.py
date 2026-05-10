a29e4f8f011686ae84b6865d37841d29113b3000
florabarberaruizdealda-crypto <florabarberaruizdealda@gmail.com>
2026-05-10 15:30:42 +0200

    Interactive Calculator

---
+++ basics.py
@@ -0,0 +1,15 @@
+print ("Interactive Cal")
+
+print("Primer Numero")
+numero1 = input ()
+
+print ("Segundo Numero")
+numero2 = input ()
+
+print(10*"-")
+numero1 = float (numero1)
+numero2 = float (numero2)
+
+result= numero1 * numero2
+
+print (result)
