fb52860b63741f622ef863dfcb00d4c210af1353
florabarberaruizdealda-crypto <florabarberaruizdealda@gmail.com>
2026-05-10 15:31:28 +0200

    Only say "hi" if you are nico or Flora

---
+++ for.py
@@ -0,0 +1,13 @@
+print ("cual es tu nombre")
+nombre = input()
+
+print("Hola " + nombre + ", ¿cómo estás?")
+
+
+
+students = ["Nico", "Flora", "Sofia", "Pablo"]
+
+for student in students:
+    print(student)
+    if nombre == "Flora":
+        print("Hola " + nombre + ", ¿cómo estás?")
